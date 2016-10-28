d8b251b46d98108b134df08046463dff3cce2d28
hambuol <oliver.hamburger@ssfs.org>
2016-10-28 10:54:04 -0400

    final, maybe check before turn in

--- account.py
+++ account.py
@@ -1,28 +1,49 @@
-class Account:
+# written by oliver hamburger
+# program is a class for a simple account
 
-    def __init__ (self, name, accountNumber, balance = 0):
+
+class Account:
+    """basic funtionality for a simple account. users can make deposits, withdraws and check their balance"""
+
+    def __init__(self, name, accountnumber, balance=0):
 
         self.name = name
-        self.accountNumber = accountNumber
+        self.accountNumber = accountnumber
         self.balance = balance
 
     def deposit(self, amount):
+        """
+        adds deposited amount to balance
+        :param:amount:
+        :return:None
+        """
         self.balance = self.balance + amount
 
     def withdraw(self, amount):
+        """
+        withdraws from account if the withdraw ammount is less than the original ammount
+        :param:withdraw
+        :return:True or False
+        """
         if amount > self.balance:
             return False
         else:
-            self.balance = self.balance - amount
+            self.balance -= amount
             return True
 
-    def showBalance(self):
+    def showbalance(self):
+        """
+        returns balance
+        :param:None
+        :return:balance
+        """
+
         return self.balance
 
     def __str__(self):
+        """
+        returns users name and current balance
+        :param:None
+        :return:name and their balance
+        """
         return self.name + "has a current balance of" + str(self.balance)
-
-
-
-
-
--- accountTesting.py
+++ accountTesting.py
@@ -1,58 +1,62 @@
+# written by oliver hamburger
+# program testes account class
+
+# imports account class
 import account
 
+# creates acount with name and given ammount
 mike = account.Account("mike", 2468, 10)
+# withdraws given ammount only if ammount is smaller than account
 if mike.withdraw(5):
-    print("your new balance is", mike.showBalance())
+    print("your new balance is", mike.showbalance())
 else:
     print("insuficient funds")
-
+# deposits given ammount into balance
 mike.deposit(50)
-print(mike.showBalance())
-
+print(mike.showbalance())
+# withdraws given ammount only if ammount is smaller than account
 if mike.withdraw(100):
-    print("your new balance is" , mike.showBalance())
+    print("your new balance is", mike.showbalance())
 else:
     print("insuficient funds")
-
+# deposits given ammount into balance
 mike.deposit(900)
-print(mike.showBalance())
+print(mike.showbalance())
 
-
+# creates acount with name and given ammount
 jim = account.Account("jim", 1359, 30)
+# withdraws given ammount only if ammount is smaller than account
 if jim.withdraw(5):
-    print("your new balance is", jim.showBalance())
+    print("your new balance is", jim.showbalance())
 else:
     print("insuficient funds")
-
+# deposits given ammount into balance
 jim.deposit(50)
-print(jim.showBalance())
-
+print(jim.showbalance())
+# withdraws given ammount only if ammount is smaller than account
 if jim.withdraw(100):
-    print("your new balance is" , jim.showBalance())
+    print("your new balance is", jim.showbalance())
 else:
     print("insuficient funds")
-
+# deposits given ammount into balance
 jim.deposit(900)
-print(jim.showBalance())
+print(jim.showbalance())
 
-
+# creates acount with name and given ammount
 bob = account.Account("bob", 1234, 70)
+# withdraws given ammount only if ammount is smaller than account
 if bob.withdraw(5):
-    print("your new balance is", bob.showBalance())
+    print("your new balance is", bob.showbalance())
 else:
     print("insuficient funds")
-
+# deposits given ammount into balance
 bob.deposit(50)
-print(bob.showBalance())
-
+print(bob.showbalance())
+# withdraws given ammount only if ammount is smaller than account
 if bob.withdraw(100):
-    print("your new balance is" , bob.showBalance())
+    print("your new balance is", bob.showbalance())
 else:
     print("insuficient funds")
-
+# deposits given ammount into balance
 bob.deposit(900)
-print(bob.showBalance())
-
-
-
-
+print(bob.showbalance())
